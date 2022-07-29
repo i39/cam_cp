4883f2069b26ec8a600f2fd51e91af7d4d09fa9c
vadim <vadim@i39.in>
2022-07-29 13:10:03 +0200

    Add options

--- app/main.go
+++ app/main.go
@@ -12,25 +12,52 @@ import (
 )
 
 var opts struct {
-	Dbg bool `long:"dbg" env:"DEBUG" description:"debug mode"`
-	Ftp struct {
+	Dbg   bool `long:"dbg" env:"DEBUG" description:"debug mode"`
+	FtpIn struct {
 		Enabled       bool          `long:"enabled" env:"ENABLED" description:"enable ftp watcher"`
 		Ip            string        `long:"ip" env:"IP" default:"127.0.0.1" description:"ip address of ftp server"`
 		User          string        `long:"user" env:"USER" default:"anonymous" description:"user name"`
 		Password      string        `long:"password" env:"PASSWORD" default:"" description:"user password"`
 		Dir           string        `long:"dir" env:"DIR" default:"/" description:"ftp directory for recursive read"`
 		CheckInterval time.Duration `long:"interval" env:"INTERVAL" default:"3s" description:"ftp check interval"`
-	} `group:"ftp" namespace:"ftp" env-namespace:"FTP"`
-	File struct {
+	} `group:"in" namespace:"ftp_in" env-namespace:"FTP_IN"`
+	FileIn struct {
 		Enabled       bool          `long:"enabled" env:"ENABLED" description:"enable file watcher"`
 		Dir           string        `long:"dir" env:"DIR" default:"/tmp" description:"file directory for recursive read"`
 		CheckInterval time.Duration `long:"interval" env:"INTERVAL" default:"3s" description:"file check interval"`
-	} `group:"file" namespace:"file" env-namespace:"FILE"`
-	Http struct {
+	} `group:"in" namespace:"file_in" env-namespace:"FILE_IN"`
+	HttpIn struct {
 		Enabled       bool          `long:"enabled" env:"ENABLED" description:"enable http watcher"`
 		Url           string        `long:"url" env:"URL" default:"http://localhost:8080" description:"http url"`
 		CheckInterval time.Duration `long:"interval" env:"INTERVAL" default:"3s" description:"http check interval"`
-	} `group:"http" namespace:"http" env-namespace:"HTTP"`
+	} `group:"in" namespace:"http_in" env-namespace:"HTTP_IN"`
+
+	Deepstack struct {
+		Enabled bool   `long:"enabled" env:"ENABLED" description:"enable deepstack filter"`
+		Url     string `long:"url" env:"URL" default:"http://localhost:8080" description:"deepstack url"`
+		ApiKey  string `long:"api-key" env:"API_KEY" default:"" description:"deepstack api key"`
+	} `group:"filters" namespace:"deepstack" env-namespace:"DEEPSTACK"`
+
+	FileOut struct {
+		Enabled bool   `long:"enabled" env:"ENABLED" description:"enable file saver"`
+		Dir     string `long:"dir" env:"DIR" default:"/tmp" description:"file directory for saving"`
+	} `group:"out" namespace:"file_out" env-namespace:"FILE_OUT"`
+	HttpOut struct {
+		Enabled bool   `long:"enabled" env:"ENABLED" description:"enable http saver"`
+		Url     string `long:"url" env:"URL" default:"http://localhost:8080" description:"http url"`
+	} `group:"out" namespace:"http_out" env-namespace:"HTTP_OUT"`
+	FtpOut struct {
+		Enabled  bool   `long:"enabled" env:"ENABLED" description:"enable ftp saver"`
+		Ip       string `long:"ip" env:"IP" default:"127.0.0.1" description:"ip address of ftp server"`
+		User     string `long:"user" env:"USER" default:"anonymous" description:"user name"`
+		Password string `long:"password" env:"PASSWORD" default:"" description:"user password"`
+		Dir      string `long:"dir" env:"DIR" default:"/" description:"ftp directory for recursive write"`
+	} `group:"out" namespace:"ftp_out" env-namespace:"FTP_OUT"`
+	TelegramOut struct {
+		Enabled bool   `long:"enabled" env:"ENABLED" description:"enable telegram filter"`
+		Token   string `long:"token" env:"TOKEN" default:"" description:"telegram token"`
+		ChatId  string `long:"chat-id" env:"CHAT_ID" default:"" description:"telegram chat id"`
+	} `group:"out" namespace:"telegram" env-namespace:"TELEGRAM"`
 }
 
 var revision = "unknown"
