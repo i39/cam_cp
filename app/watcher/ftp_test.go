c3be8497aa7b7c10a28fc06644c6a8a8a3620c44
vadim <vadim@i39.in>
2022-08-01 15:17:02 +0200

    Intergration test for ftp

--- app/watcher/ftp_test.go
+++ app/watcher/ftp_test.go
@@ -1,32 +1,104 @@
 package watcher
 
 import (
+	"bytes"
 	"github.com/jlaffaye/ftp"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 	"time"
 )
 
 func TestWalkFtp(t *testing.T) {
+	//use dlptest for testing ftp server
+	//https://dlptest.com/ftp-test/ for detailed information
+
+	createTempFiles(t)
 
 	f := &Ftp{
-		Dir:           "/test/",
+		Dir:           "/cam_cp/",
 		CheckInterval: time.Second * 10,
-		Ip:            "192.168.89.236:21",
-		User:          "ftpuser",
-		Password:      "vad6Udkh",
-	}
-	c, err := ftp.Dial(f.Ip, ftp.DialWithTimeout(time.Second*10))
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = c.Login(f.User, f.Password)
-	if err != nil {
-		t.Fatal(err)
+		Ip:            "ftp.dlptest.com",
+		User:          "dlpuser",
+		Password:      "rNrKYTX9g7z3RgJRmxWuGHbeu",
 	}
 
 	files, err := f.walkFtp()
-	assert.Equal(t, "/test/test2/test3/test3.txt", files[0])
-	assert.Equal(t, "/test/test2/test2.txt", files[1])
-	assert.Equal(t, "/test/test1/test1.txt", files[2])
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, "/cam_cp/test2/test3/test3.txt", files[0])
+	assert.Equal(t, "/cam_cp/test2/test2.txt", files[1])
+	assert.Equal(t, "/cam_cp/test1.txt", files[2])
+	deleteTestFiles(t)
+}
+
+func createTempFiles(t *testing.T) {
+	c, err := ftp.Dial("ftp.dlptest.com:21", ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = c.Login("dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = c.MakeDir("/cam_cp")
+	if err != nil {
+		t.Error(err)
+	}
+	err = c.MakeDir("/cam_cp/test2")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = c.MakeDir("/cam_cp/test2/test3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	var data *bytes.Buffer
+	data = bytes.NewBufferString("test1")
+	err = c.Stor("/cam_cp/test1.txt", data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	data = bytes.NewBufferString("test2")
+	err = c.Stor("/cam_cp/test2/test2.txt", data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	data = bytes.NewBufferString("test3")
+	err = c.Stor("/cam_cp/test2/test3/test3.txt", data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := c.Quit(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func deleteTestFiles(t *testing.T) {
+	c, err := ftp.Dial("ftp.dlptest.com:21", ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = c.Login("dlpuser", "rNrKYTX9g7z3RgJRmxWuGHbeu")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = c.RemoveDirRecur("/cam_cp")
+	if err != nil {
+		t.Error(err)
+	}
+	if err := c.Quit(); err != nil {
+		log.Fatal(err)
+	}
 }
